0fda59b2fc82bbfd5c7e7cb449209e04cb269a3d
Zeno <r1102011@ucll.be>
2025-12-17 10:30:51 +0100

    change

--- main.py
+++ main.py
@@ -48,28 +48,28 @@ clock = pygame.time.Clock()
 font = pygame.font.SysFont("Arial", 32)
 big_font = pygame.font.SysFont("Arial", 64)
 
-# Audio - commented out due to missing sound files
-# background_music = "assets/sounds/background_music.mp3"
-# musquito_sound = pygame.mixer.Sound("assets/sounds/musquito_sound.mp3")
-# frog_sound = pygame.mixer.Sound("assets/sounds/frog_sound.mp3")
-# tongue_attack = pygame.mixer.Sound("assets/sounds/tongue_stretching.mp3")
-# game_over_sound = pygame.mixer.Sound("assets/sounds/fail_sound.mp3")
-# level_up = pygame.mixer.Sound("assets/sounds/lvl_up_sound.mp3")
-# victory = pygame.mixer.Sound("assets/sounds/victory_sound.mp3")
-# death_sound = pygame.mixer.Sound("assets/sounds/death_sound.mp3")
-# hit_sound = pygame.mixer.Sound("assets/sounds/hit_sound.mp3")
-# suck_sound = pygame.mixer.Sound("assets/sounds/suck_sound.mp3")
-# slap_sound = pygame.mixer.Sound("assets/sounds/slap_sound.mp3")
-# countdown_sound = pygame.mixer.Sound("assets/sounds/countdown.mp3")
+ #Audio
+ background_music = "assets/sounds/background_music.mp3"
+ musquito_sound = pygame.mixer.Sound("assets/sounds/musquito_sound.mp3")
+ frog_sound = pygame.mixer.Sound("assets/sounds/frog_sound.mp3")
+ tongue_attack = pygame.mixer.Sound("assets/sounds/tongue_stretching.mp3")
+ game_over_sound = pygame.mixer.Sound("assets/sounds/fail_sound.mp3")
+ level_up = pygame.mixer.Sound("assets/sounds/lvl_up_sound.mp3")
+ victory = pygame.mixer.Sound("assets/sounds/victory_sound.mp3")
+ death_sound = pygame.mixer.Sound("assets/sounds/death_sound.mp3")
+ hit_sound = pygame.mixer.Sound("assets/sounds/hit_sound.mp3")
+ suck_sound = pygame.mixer.Sound("assets/sounds/suck_sound.mp3")
+ slap_sound = pygame.mixer.Sound("assets/sounds/slap_sound.mp3")
+ countdown_sound = pygame.mixer.Sound("assets/sounds/countdown.mp3")
 
-# pygame.mixer.music.load(background_music)
-# pygame.mixer.music.set_volume(0.5)
-# pygame.mixer.music.play(-1)  # -1 = loop forever
+ pygame.mixer.music.load(background_music)
+ pygame.mixer.music.set_volume(0.5)
+ pygame.mixer.music.play(-1)  # -1 = loop forever
 
-# musquito_sound.set_volume(0.3)
-# musquito_sound.play(-1)
-# frog_sound.set_volume(0.3)
-# frog_sound.play(-1)
+ musquito_sound.set_volume(0.3)
+ musquito_sound.play(-1)
+ frog_sound.set_volume(0.3)
+ frog_sound.play(-1)
 
 import images
 
