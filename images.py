ea597be58a0d452d31267ddcbae06320cf205531
Jonas1010ucll <r1094165@ucll.be>
2025-12-17 09:45:39 +0100

    frog

--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 import pygame
 
 # Load and scale images
-frog_image = pygame.image.load('assets/images/frog1.png').convert_alpha()
+frog_image = pygame.image.load('assets/images/kikker_zonder_tong.png').convert_alpha()
 frog_image = pygame.transform.scale(frog_image, (230, 170))
 
 mosquito_image = pygame.image.load('assets/images/fly_sprite.png').convert_alpha()
